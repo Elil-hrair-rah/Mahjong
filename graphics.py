91a25690c8327e7ae7372d6b7ea434ee8f8002fc
unknown <a.he@mail.utoronto.ca>
2020-10-20 02:40:45 -0400

    logic for determining and sorting rotated tiles

--- graphics.py
+++ graphics.py
@@ -23,13 +23,36 @@ from mahjong.constants import EAST, SOUTH, WEST, NORTH
 from player import Player
 from tiles import Tile, Tiles, Wall, OneOfEach, Dora, Hand, Meld, Melds, Discards
 
+''' testing code
+
+alsonotbob = Player('anb',3)
+bob = Player('bob', 1)
+notbob = Player('notbob',2)
+bob.hand.add_tiles('11p23s5505m666p777s123z')
+tile1 = Tile('1','p')
+tile2 = Tile('1','s')
+tile3 = Tile('6','p')
+tile4 = Tile('7','s')
+bob.seat = SOUTH
+game = Game([notbob, alsonotbob, alsonotbob, alsonotbob])
+bob.pon(tile1, game)
+bob.chii(tile2, game)
+bob.seat = NORTH
+bob.ckan()
+bob.okan(tile3, game)
+bob.seat = WEST
+bob.pon(tile4, game)
+bob.ckan()
+
+'''
+
 def player_image(player, *args):
     if not isinstance(player, Player):
         return False
     
     if player.melds.melds:
         image_list = []
-        parts = [player.hand]
+        parts = [str(player.hand)]
         rotated = []
         rotated.append([0 for tile in player.hand.tiles])
         for arg in args:
@@ -38,12 +61,70 @@ def player_image(player, *args):
         for meld in player.melds.melds:
             rotation = {'L': [1, 0, 0], 'M': [0, 1, 0], 'R': [0, 0, 1]}
             if meld.who:
+                
                 rotate = rotation[meld.who][:]
                 if meld.shominkan:
                     rotate[rotate.index(1):rotate.index(1)+1] = [1,2]
                 elif len(meld) == 4:
                     rotate.insert(2, 0)
-                print(rotate)
+                    
+
+                tiles = sorted(meld.tiles, key=lambda tile:tile.true_value)
+                remove = [index for index, tile in enumerate(tiles) if tile.suit == meld.called.suit and tile.value == meld.called.value]
+                del tiles[remove[0]]                
+                string = ''
+                zero_counter = 0
+                
+                for ref in rotate:
+                    if ref == 0:
+                        string += tiles[zero_counter].value
+                        zero_counter += 1
+                    elif ref == 1:
+                        string += meld.called.value
+                    elif ref == 2:
+                        string += meld.shominkan_tile.value
+                string += meld.called.suit
+                    
+            else:
+                rotate = [0 for tile in meld.tiles]
+                string = str(meld)
+                if string[0] == string[3]:
+                    string = 'x'.join(string.rsplit(meld.tiles[0].value, 1))
+                    string = string.replace(meld.tiles[0].value, 'x', 1)
+                elif string[0] == string[1]:
+                    string = 'x'.join(string.rsplit(meld.tiles[0].value, 2))
+                else:
+                    string = string.replace(meld.tiles[-1].value, 'x', 2)
+                    
+            rotated.append(rotate)
+            parts.append(string)
+        #return rotated, parts
+        
+        image_list = []
+        for part in parts:
+            results = re.findall(r'([0-9x]+[mpsz])', part)
+            if part == parts[0]:
+                results = list(
+                    reduce(list.__add__,
+                           [['{}{}'.format(x, result[-1]) for x in result[:-1]]
+                            for result in results]))
+            for result in results:
+                image_list += [
+                    './ui/{}{}.png'.format(x, result[-1]) for x in result[:-1]
+                ]
+            image_list.append('./ui/0.png')
+        
+        ref = []
+        for block in rotated:
+            ref.extend(block)
+            ref.append(0)
+        del ref[-1]
+        
+        #return image_list, ref
+        
+        for image, refs in zip(image_list, ref):
+            print(image, refs)
+            
             
         
     else:
